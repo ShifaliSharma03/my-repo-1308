4f955fc43fad49b400123c8a40a2ec99c4c1fd72
Shifali03 <sharma.shifali12@gmail.com>
2025-08-13 15:50:43 +0530

    Added HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+import time
+import sys
+
+message = "Hello World"
+for char in message:
+    print(char, end='', flush=True)
+    time.sleep(0.2)
+print()  # Move to next line after animation
+
+#Write a code to generate a star like pattern
+def print_star_pattern(n):
+    for i in range(n):
+        print(' ' * (n - i - 1) + '*' * (2 * i + 1))            
+print_star_pattern(5)   
